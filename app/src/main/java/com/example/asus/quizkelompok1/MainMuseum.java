cf7badacac66d293aa64d4202e2cee5ec625bb2f
Yudha Pradana <yudhapradana102@gmail.com>
2018-10-01 13:44:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/asus/quizkelompok1/MainMuseum.java
@@ -0,0 +1,13 @@
+package com.example.asus.quizkelompok1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMuseum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_museum);
+    }
+}
